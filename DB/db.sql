084b5cd2e9f0728f03b73d555c5e870026dab7d2
Jean0405 <jeanpierrea0405@gmail.com>
2023-07-23 16:01:19 -0500

    feat: :card_file_box: Se hacen querys de prueba
    Se organizan bien los querys de la DB para un uso intuitivo a la hora de realizar pruebas de calificación

--- DB/db.sql
+++ DB/db.sql
@@ -1,6 +1,10 @@
+/*----------------------CREACIÓN DE LA BASE DE DATOS------------------------*/
+
 CREATE DATABASE Synthia;
 
 USE Synthia;
+
+/*---------------------CREACIÓN DE LAS TABLAS-----------------------*/
 
 CREATE TABLE
     roles (
@@ -23,8 +27,6 @@ CREATE TABLE
         id_usuario INT,
         id_modulo INT
     );
-
-DROP TABLE modulos_usuarios;
 
 CREATE TABLE
     comentarios(
@@ -71,6 +73,8 @@ CREATE TABLE
         id_estado INT
     );
 
+/*----------------------CREANDO LAS RELACIONES ENTRE TABLAS--------------------*/
+
 ALTER TABLE usuarios
 ADD
     CONSTRAINT fk_roles FOREIGN KEY (id_rol) REFERENCES roles(id);
@@ -103,138 +107,98 @@ ADD
 ADD
     CONSTRAINT fk_estados_proEstados FOREIGN KEY (id_estado) REFERENCES estados(id) ON DELETE CASCADE;
 
+/*¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ IMPORTANTE !!!!!!!!!!!!!!!!!!*/
+
+/*----------------------CREANDO LOS ROLES POR DEFECTO (Deben ser los unicos que existan en la DB)----------------------*/
+
 INSERT INTO roles(nombre) VALUES ("admin"),("desarrollador");
 
-INSERT INTO
-    usuarios(
-        id,
-        nombre,
-        email,
-        contrasena,
-        id_rol
-    )
-VALUES (
-        1005111333,
-        "Hilter",
-        "hitler@gmail.com",
-        "hitler",
-        2
-    );
+/*--------QUERYS DE PRUEBA--------*/
 
-INSERT INTO
-    proyectos (
-        nombre,
-        descripcion,
-        fecha_creacion
-    )
-VALUES (
-        "Ecommerce tienda churros",
-        "bla bla bla bla",
-        "2023-07-19"
-    );
+/* 
+ SELECT * FROM usuarios;
+ SELECT * FROM proyectos;
+ SELECT * FROM modulos;
+ SELECT * FROM modulos_usuarios;
+ SELECT * FROM comentarios;
+ SELECT * FROM estados;
+ SELECT * FROM proyectos_estados;
+ SELECT * FROM modulos_estados; */
 
-/*QUERYS DE PRUEBA*/
-
-SELECT * FROM usuarios;
-
-SELECT * FROM proyectos;
-
-SELECT * FROM modulos;
-
-SELECT * FROM modulos_usuarios;
-
-SELECT * FROM comentarios;
-
-SELECT * FROM estados;
-
-SELECT * FROM proyectos_estados;
-
-SELECT * FROM modulos_estados;
-
-SELECT
-    modulos.id,
-    estados.nombre AS estado,
-    modulos.nombre AS modulo,
-    modulos.descripcion,
-    proyectos.nombre AS nombre_proyecto
-FROM modulos_estados AS ms
-    INNER JOIN modulos ON ms.id_modulo = modulos.id
-    INNER JOIN estados ON ms.id_estado = estados.id
-    INNER JOIN proyectos ON modulos.id_proyecto = proyectos.id;
-
-SELECT
-    estados.nombre AS estado,
-    proyectos.nombre AS proyecto,
-    proyectos.descripcion,
-    proyectos.fecha_creacion
-FROM proyectos_estados
-    INNER JOIN proyectos ON proyectos_estados.id_proyecto = proyectos.id
-    INNER JOIN estados ON proyectos_estados.id_estado = estados.id;
-
-SELECT
-    modulos.nombre AS modulo,
-    c.contenido AS comentario,
-    c.fecha_creacion AS fecha,
-    usuarios.id AS id_usuario,
-    usuarios.nombre AS usuario
-FROM comentarios AS c
-    INNER JOIN usuarios ON c.id_usuario = usuarios.id
-    INNER JOIN modulos ON c.id_modulo = modulos.id
-WHERE modulos.id = 2;
-
-SELECT
-    roles.nombre AS rol,
-    usuarios.nombre AS usuario
-FROM modulos_usuarios AS ms
-    INNER JOIN usuarios ON ms.id_usuario = usuarios.id
-    INNER JOIN roles ON usuarios.id_rol = roles.id
-WHERE
-    ms.id_usuario = 1005372573
-    AND ms.id_modulo = 1;
-
-SELECT
-    modulos.id,
-    modulos.nombre AS modulo,
-    proyectos.nombre AS proyecto,
-    usuarios.id AS id_usuario,
-    usuarios.nombre AS usuarios_asignados
-FROM modulos_usuarios AS ms
-    INNER JOIN usuarios ON ms.id_usuario = usuarios.id
-    INNER JOIN modulos ON ms.id_modulo = modulos.id
-    INNER JOIN proyectos ON modulos.id_proyecto = proyectos.id
-WHERE modulos.id = 3;
-
-SELECT
-    usuarios.id,
-    usuarios.nombre,
-    usuarios.email,
-    roles.nombre AS rol
-FROM usuarios
-    INNER JOIN roles ON usuarios.id_rol = roles.id
-WHERE usuarios.id = 1005184201;
-
-SELECT COUNT(*) AS total FROM proyectos WHERE id = 4;
-
-SELECT
-    modulos.id AS id_modulo,
-    modulos.nombre AS modulo,
-    modulos.descripcion AS descripcion,
-    proyectos.nombre AS nombre_proyecto
-FROM modulos
-    INNER JOIN proyectos ON modulos.id_proyecto = proyectos.id;
-
-SELECT
-    usuarios.*,
-    roles.nombre AS rol
-FROM usuarios
-    INNER JOIN roles ON usuarios.id_rol = roles.id
-WHERE usuarios.id = 1005184201;
-
-SELECT * FROM modulos WHERE id = 1;
-
-UPDATE modulos
-SET
-    nombre = "Crear ENDPOINTS",
-    descripcion = "blah",
-    id_proyecto = 1
-WHERE id = 3;
+/* SELECT
+ modulos.id,
+ estados.nombre AS estado,
+ modulos.nombre AS modulo,
+ modulos.descripcion,
+ proyectos.nombre AS nombre_proyecto
+ FROM modulos_estados AS ms
+ INNER JOIN modulos ON ms.id_modulo = modulos.id
+ INNER JOIN estados ON ms.id_estado = estados.id
+ INNER JOIN proyectos ON modulos.id_proyecto = proyectos.id;
+ SELECT
+ estados.nombre AS estado,
+ proyectos.nombre AS proyecto,
+ proyectos.descripcion,
+ proyectos.fecha_creacion
+ FROM proyectos_estados
+ INNER JOIN proyectos ON proyectos_estados.id_proyecto = proyectos.id
+ INNER JOIN estados ON proyectos_estados.id_estado = estados.id;
+ SELECT
+ modulos.nombre AS modulo,
+ c.contenido AS comentario,
+ c.fecha_creacion AS fecha,
+ usuarios.id AS id_usuario,
+ usuarios.nombre AS usuario
+ FROM comentarios AS c
+ INNER JOIN usuarios ON c.id_usuario = usuarios.id
+ INNER JOIN modulos ON c.id_modulo = modulos.id
+ WHERE modulos.id = 2;
+ SELECT
+ roles.nombre AS rol,
+ usuarios.nombre AS usuario
+ FROM modulos_usuarios AS ms
+ INNER JOIN usuarios ON ms.id_usuario = usuarios.id
+ INNER JOIN roles ON usuarios.id_rol = roles.id
+ WHERE
+ ms.id_usuario = 1005372573
+ AND ms.id_modulo = 1;
+ SELECT
+ modulos.id,
+ modulos.nombre AS modulo,
+ proyectos.nombre AS proyecto,
+ usuarios.id AS id_usuario,
+ usuarios.nombre AS usuarios_asignados
+ FROM modulos_usuarios AS ms
+ INNER JOIN usuarios ON ms.id_usuario = usuarios.id
+ INNER JOIN modulos ON ms.id_modulo = modulos.id
+ INNER JOIN proyectos ON modulos.id_proyecto = proyectos.id
+ WHERE modulos.id = 3;
+ SELECT
+ usuarios.id,
+ usuarios.nombre,
+ usuarios.email,
+ roles.nombre AS rol
+ FROM usuarios
+ INNER JOIN roles ON usuarios.id_rol = roles.id
+ WHERE usuarios.id = 1005184201;
+ SELECT COUNT(*) AS total FROM proyectos WHERE id = 4;
+ SELECT
+ modulos.id AS id_modulo,
+ modulos.nombre AS modulo,
+ modulos.descripcion AS descripcion,
+ proyectos.nombre AS nombre_proyecto
+ FROM modulos
+ INNER JOIN proyectos ON modulos.id_proyecto = proyectos.id;
+ SELECT
+ usuarios.*,
+ roles.nombre AS rol
+ FROM usuarios
+ INNER JOIN roles ON usuarios.id_rol = roles.id
+ WHERE usuarios.id = 1005184201;
+ SELECT * FROM modulos WHERE id = 1;
+ UPDATE modulos
+ SET
+ nombre = "Crear ENDPOINTS",
+ descripcion = "blah",
+ id_proyecto = 1
+ WHERE id = 3; */
